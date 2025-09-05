958c565f86c0b0ed3fb59f760cb79d7454ca91af
numerconstantino07 <numerconstantino@gmail.com>
2025-09-05 21:00:42 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,22 @@
+
+let name = "Numer";      
+let age = 25;            
+let isStudent = true;  
+
+
+console.log("Name:", name, "| Type:", typeof name);
+console.log("Age:", age, "| Type:", typeof age);
+console.log("Is Student:", isStudent, "| Type:", typeof isStudent);
+
+
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+
+
+console.log('"5" == 5:', "5" == 5);  
+console.log('"5" === 5:', "5" === 5); 
