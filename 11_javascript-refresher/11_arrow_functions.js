9bd89fb9e1dcd99749fc54e5fdcd6f0a973e6c11
numerconstantino07 <numerconstantino@gmail.com>
2025-09-06 09:10:44 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,12 @@
+const greet = (name) => {
+  return "Hello, " + name;
+};
+console.log(greet("Numer"));
+
+const square = n => n * n;
+console.log("Square of 7:", square(7));
+
+const sayHi = () => {
+  console.log("Hi!");
+};
+sayHi();
