3d5612f621089e8da755824d6fda050ef86215b4
numerconstantino07 <numerconstantino@gmail.com>
2025-10-26 00:05:40 +0800

    Done with how to render multiple components at a time

---
+++ react-describing-ui/src/assets/data.js
@@ -0,0 +1,23 @@
+export const people = [
+  {
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MK3eW3A'
+  },
+  {
+    id: 1,
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'mynHUSa'
+  },
+  {
+    id: 2,
+    name: 'Mohammad Abdus Salam',
+    profession: 'physicist',
+    accomplishment: 'electromagnetism theory',
+    imageId: 'bE7W1ji'
+  }
+];
