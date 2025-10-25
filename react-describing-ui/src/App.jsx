fd4410b89e27c5bd75f43d74b889fcb51334416f
numerconstantino07 <numerconstantino@gmail.com>
2025-10-25 23:59:25 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -17,3 +17,29 @@ export default function Gallery() {
     </section>
   );
 }
+
+import Gallery from './Gallery';
+
+export default function App() {
+  return <Gallery />;
+}
+
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
+  );
+}
+
+
