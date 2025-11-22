bff9409c3a6036e891b633537ca8fad7bfc29434
numerconstantino07 <numerconstantino@gmail.com>
2025-11-22 14:40:13 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,8 +1,22 @@
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>"Hello World inside quotes"</h1>
+//       <p>"This is a string with quotation marks inside JSX"</p>
+//     </div>
+//   );
+// }
+
+const name = "Juan";
+const age = 20;
+
 export default function App() {
   return (
     <div>
-      <h1>"Hello World inside quotes"</h1>
-      <p>"This is a string with quotation marks inside JSX"</p>
+      <h1>Hello, {name}!</h1>
+      <p>You are {age} years old.</p>
+      <p>2 + 2 = {2 + 2}</p>
+      <p>Your age next year: {age + 1}</p>
     </div>
   );
 }
